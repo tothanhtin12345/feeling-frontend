7eed6e7d38ac4bf551ef05b38cd2158e99675dc6
Thanh Tin <tothanhtin12345@gmail.com>
2022-02-06 20:23:56 +0700

    add logo and edit module exports cost

--- src/store/modules/JoinGroupRequestList/methods.js
+++ src/store/modules/JoinGroupRequestList/methods.js
@@ -1,8 +1,8 @@
-module.exports.setCardButtonLoadingMethod = (state, userId, loadingVal) => {
 
-  //tìm user đó ra
-  const userIndex = state.requestList.findIndex((item) => item._id === userId);
-  if (userIndex >= 0) {
-    state.requestList[userIndex].loading = loadingVal;
-  }
-};
+export const setCardButtonLoadingMethod = (state, userId, loadingVal) => {
+   //tìm user đó ra
+   const userIndex = state.requestList.findIndex((item) => item._id === userId);
+   if (userIndex >= 0) {
+     state.requestList[userIndex].loading = loadingVal;
+   }
+}
